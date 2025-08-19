c47995a1558cc5acbf7127d328cc8352bd57b016
pedrocanute <pedro.canute2@gmail.com>
2025-08-19 14:12:13 -0300

    Explicando linhas de código do Exercicio04_1

--- Lista 02/Lista 02/Exercicio04_1.c
+++ Lista 02/Lista 02/Exercicio04_1.c
@@ -5,13 +5,13 @@
 #include <math.h>
 #include <time.h>
 
-typedef struct {
+typedef struct { //criando a struct Pessoa
 	char nome[50];
 	int idade;
 }Pessoa;
 
-void imprimirPessoa(char nome[], int indice, Pessoa listaPessoa[]) {
-	if (strcmp(nome, listaPessoa[indice].nome) == 0) {
+void imprimirPessoa(char nome[], int indice, Pessoa listaPessoa[]) { //função com argumento nome[](recebido da Main) indice (numero da posiçao no array) e o array listaPessoa[]
+	if (strcmp(nome, listaPessoa[indice].nome) == 0) {//strcmp compara as strings se forem identicas, retorna 0
 		printf("Nome: %s | Idade: %d.", listaPessoa[indice].nome, listaPessoa[indice].idade);
 	}
 }
@@ -20,12 +20,12 @@ int main() {
 	srand(time(NULL));
 	/*VERSAO USANDO NOME */
 	/*Crie um array de Pessoa, e inclua 3 pessoas.Depois, crie uma função que recebe um nome, procure a pessoa pelo nome no array e imprima seus dados.*/
-	Pessoa listaPessoa[3] = { {.nome = "Pedro", .idade = 29}, {.nome = "Vitor", .idade = 32}, {.nome = "Paulo", .idade = 20} };
-	int indice = rand() % 3;
+	Pessoa listaPessoa[3] = { {.nome = "Pedro", .idade = 29}, {.nome = "Vitor", .idade = 32}, {.nome = "Paulo", .idade = 20} };//atribui nome e idade para cada Pessoa no array de structs Pessoa
+	int indice = rand() % 3;//sorteia de 0 a 2 o indice
 	char nome[50];
-	strcpy(nome, listaPessoa[indice].nome);
+	strcpy(nome, listaPessoa[indice].nome);//copia a string que esta na variavel nome da array de pessoas de acordo com o indice sorteado para a variavel nome na main
 
-	imprimirPessoa(nome, indice, listaPessoa);
+	imprimirPessoa(nome, indice, listaPessoa);// chama a funçao enviando nome, indice e o array de structs
 
 	return 0;
 }
